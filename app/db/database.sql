01c1bf65f4f85830423546b16e550834eeb7afde
Largo <largoko@gmail.com>
2017-11-12 01:02:02 +0900

    whoops forgot database

---
+++ app/db/database.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 11. Nov 2017 um 17:00
+-- Server Version: 5.5.58-0ubuntu0.14.04.1
+-- PHP-Version: 5.6.20-1+deb.sury.org~trusty+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Datenbank: `lastfmtwitter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `lastfmname` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `twittertext` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=24 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `hash` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `subscription` date DEFAULT NULL,
+  `inactive` tinyint(1) NOT NULL DEFAULT '0',
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `forgotPasswordChallenge` mediumblob,
+  `oauth_access_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth_access_token_secret` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastTwitterName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
